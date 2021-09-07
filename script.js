66400e3297857718b5ce6055d0e78dd7e95752a9
Milind-dev <milinddev1011@gmail.com>
2021-09-07 13:29:16 +0530

    var__let__const

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var fruits = 'mango';
   var fruits = 'orange';
 }
 console.log(fruits);
-
+// ----------------------
 var sports = 'cricket';
 function sportmans() {
   var sports = 'football';
@@ -12,4 +12,31 @@ function sportmans() {
 }
 sportmans();
 console.log(sports);
+// -----------------------
 // var respects function scopes. So the var inside the function is limited to the scope of the function, while the var outside the function is in the global namespace.
+
+let propertyBuild = 'TerraceAppartment';
+{
+  let propertyBuild = 'RadheAppartment';
+  console.log(propertyBuild);
+}
+console.log(propertyBuild);
+// --------------------
+// var has a concept of hoisting.
+
+// When you declare something using var, it first initializes the variables with the value of undefined to the top of the scope.
+var country = 'india';
+var country = 'china';
+console.log(country);
+// ------------
+let state = undefined;
+state = 'Bangalore';
+state = 'Bangalore';
+
+// -----------
+b = 23;
+var b;
+e = 32;
+let e;
+console.log(b);
+console.log(e);
