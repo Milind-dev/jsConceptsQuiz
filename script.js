d256d3f23a82a60c3f4994289bacfa56d3c161e7
Milind-dev <milinddev1011@gmail.com>
2021-09-07 13:19:13 +0530

    global_namespace

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// var does not respect block scopes {}.
 var fruits = 'mango';
 {
-  let fruits = 'orange';
+  var fruits = 'orange';
 }
 console.log(fruits);
+
+var sports = 'cricket';
+function sportmans() {
+  var sports = 'football';
+  console.log(sports);
+}
+sportmans();
+console.log(sports);
+// var respects function scopes. So the var inside the function is limited to the scope of the function, while the var outside the function is in the global namespace.
